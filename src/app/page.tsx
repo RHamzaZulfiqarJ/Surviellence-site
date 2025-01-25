4563a3a9c6cf407255f3eb6dd4ae314ad6d63ba4
Hamza Zulfiqar <hamzazulfiqar123123@gmail.com>
2025-01-25 15:02:59 +0500

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import { useRouter } from 'next/navigation';
 
-const page = () => {
+const Page = () => {
 
   const router = useRouter();
 
@@ -12,8 +12,10 @@ const page = () => {
   }, []);
 
   return (
-    <div></div>
+    <div>
+      
+    </div>
   )
 }
 
-export default page
+export default Page
