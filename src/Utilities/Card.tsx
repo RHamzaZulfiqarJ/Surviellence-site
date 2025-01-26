a842e5e433cf92fc15e08e3b872eb640d418bd9a
moadchishti <150439399+moadchishti@users.noreply.github.com>
2025-01-26 15:44:26 +0500

    Update Card.tsx

--- src/Utilities/Card.tsx
+++ src/Utilities/Card.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const CCard = ({text, title}: {text: string, title: string}) => {
+const CCard = ({ text, title }: { text: string; title: string }) => {
   return (
-    <div className="border border-white p-4 m-4 hover:text-black hover:bg-white hover:border-black transition-all duration-300 cursor-pointer">
-        <div className="text-2xl font-bold">{title}</div>
-        <div className="">{text}</div>
+    <div className="border border-white p-4 m-4 rounded-2xl hover:text-black hover:bg-white hover:border-black transition-all duration-300 cursor-pointer">
+      <div className="text-2xl font-bold">{title}</div>
+      <div>{text}</div>
     </div>
   )
 }
 
-export default CCard
+export default CCard;
